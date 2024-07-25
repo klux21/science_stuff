854a8f9a29f72466b0931ebf6509fb82bc25a921
Dipl.-Ing. Klaus Lux <klux21@gmx.de>
2024-07-25 03:41:27 +0000

    printing of the values of several angles and the related day added

--- shapiro.c
+++ shapiro.c
@@ -88,40 +88,62 @@ exit $?
 #include <stdio.h>
 #include <math.h>
 
-const double AE         = 149597870700.0;/* astromic unit in m */
-const double c          = 299792458.0;   /* speed of light m/s */
-const double G          = 6.6743e-11;    /* Newtonian const of gravitation m³/kg/s² */
-const double m_Sun      = 1.9884e30;     /* mass of sun in kg */
-const double r_Sun      = 696342000;     /* equatorial radius of sun */
+const double  year       = 31556952.0;     /* a year of 365.2425 days in seconds */
+const double  day        = 86400.0;        /* a day in seconds */
+const double  AE         = 149597870700.0; /* astromic unit in m */
+const double  c          = 299792458.0;    /* speed of light m/s */
+const double  G          = 6.6743e-11;     /* Newtonian const of gravitation m³/kg/s² */
+const double  m_Sun      = 1.9884e30;      /* mass of sun in kg */
+const double  r_Sun      = 696342000;      /* equatorial radius of sun */
 
-const double m_Earth    = 5.972168E24;   /* mass of Earth */
-const double r_Earth    = 6378137.0;     /* equatorial radius of Earth */
-const double r_Venus    = 6051800.0;     /* equatorial radius of Venus */
-const double rb_Mercury = 0.387098 * AE; /* average radius of orbit of Mercury */
-const double rb_Pluto   = 39.482   * AE; /* average radius of orbit of Pluto */
-const double rb_Venus   = 0.7233   * AE; /* average radius of orbit of Venus */
-const double rb_Earth   = AE;            /* average radius of orbit of Earth */
+const double  m_Earth    = 5.972168E24;    /* mass of Earth */
+const double  r_Earth    = 6378137.0;      /* equatorial radius of Earth */
+const double  r_Venus    = 6051800.0;      /* equatorial radius of Venus */
+const double  rb_Mercury = 0.387098 * AE;  /* average radius of orbit of Mercury */
+const double  rb_Pluto   = 39.482   * AE;  /* average radius of orbit of Pluto */
+const double  rb_Venus   = 0.7233   * AE;  /* average radius of orbit of Venus */
+const double  rb_Earth   = AE;             /* average radius of orbit of Earth */
 
 
 int CalculateShapiro()
 {
    int iret = 0;
 
-   double dist_Sun = r_Sun + r_Venus;
-   double v2_Earth = 2.0 * G * m_Sun / rb_Earth + 2.0 * G * m_Earth / r_Earth;
+   double vb_Venus = sqrt(G * m_Sun / rb_Venus);            /* orbital velocity of Venus */
+   double vb_Earth = sqrt(G * m_Sun / rb_Earth);            /* orbital velocity of Earth */
+   double y_Venus  = 2.0 * M_PI * rb_Venus / vb_Venus;      /* calculated year of Venus */
+   double y_Earth  = 2.0 * M_PI * rb_Earth / vb_Earth;      /* calculated year of Earth */
+   double ys_Venus = 1.0 / (1.0 / y_Venus - 1.0 / y_Earth); /* synodic year of Venus (time it takes for Venus move around the sun from view of Earth) */
+   double dist_Sun = r_Sun + r_Venus / 2.0;                 /* minimal distance from sun the radar may work; */
+   double arc      = M_PI / 2.0 - asin(dist_Sun / rb_Venus) + M_PI / 2.0 - asin(dist_Sun / rb_Earth); /* angle between Earth and Venus from view of sun */
+
+   double v2_Earth = 2.0 * G * m_Sun / rb_Earth + 2.0 * G * m_Earth / r_Earth;  /*  */
    double r = 0.0;
+   double days     = 0;
+
+   printf("Synodic year of Venus in calculation=%.3f years (%.4f days)\n", ys_Venus / year, ys_Venus / day);   
+   printf("Starting angle is %.4f° for passing the Sun at %.4f times the radius of the Sun\n", arc * 360.0 / 2.0 / M_PI, dist_Sun/r_Sun);   
    
    /* calculation of Shapiro delay */
-   while(dist_Sun <= rb_Venus)
+   while(arc >= 0.0)
    {
-       double dist_max = rb_Venus * rb_Venus - dist_Sun * dist_Sun;
        double sum = 0.0;
        double diff_sum = 0.0; /* difference if using lowered tanential speed */
+       double dist_max = 0.0;
+       double dist_Venus = sqrt(rb_Venus * rb_Venus + rb_Earth * rb_Earth - 2.0 * rb_Venus * rb_Earth * cos(arc)); /* distance of Venus to according the law of cosines */
 
-       if(dist_max <= 0.0)
-          dist_max = 0.0; /* ensure this */
+       dist_Sun = sin(arc) / dist_Venus * rb_Earth * rb_Venus;
+
+       printf("\n");
+       /* printf("sin(as)=%.5f sin(av)=%.5f  rmax=%.3f times r_Sun\n", sin(arc), sin(arc) / dist_Venus * rb_Earth, rb_Venus / r_Sun); */
+       printf("%.0f days after starting day. Angle is %.4f°\n", days, arc * 360.0 / 2.0 / M_PI);
+       printf("distance of Venus is %.0f km\n", dist_Venus / 1000.0);
+       printf("%s the Sun at %.4f times the radius of the Sun\n", arc >= (0.5 * M_PI) ? "passing" : "would passing", dist_Sun/r_Sun);   
+
+       if(arc <= (0.5 * M_PI))
+          dist_max = 0.0; /* we need to skip this */
        else
-          dist_max = sqrt(dist_max);
+          dist_max = sqrt(rb_Venus * rb_Venus - dist_Sun * dist_Sun);
 
        r = dist_max;
        while(r > 0.0)
@@ -131,7 +153,7 @@ int CalculateShapiro()
           double sqr_v2  = 2.0 * G * m_Sun / dist - v2_Earth; /* escape velocity at current position from sun */
           sum += 1000.0 * (sqr_v2 / (c*c - sqr_v2)); /* length increasement because of Lorentz factor */
 
-          if(sqr_v2 > 0)
+          if(sqr_v2 > 0.0)
              diff_sum += 1000.0 * dist_Sun / dist * (sqrt(1.0 + sqr_v2 / (c*c - sqr_v2)) - 1.0);
 
           if(r > 1000.0)
@@ -148,6 +170,10 @@ int CalculateShapiro()
           dist_max = sqrt(dist_max);
 
        r = dist_max;
+
+       if(arc < 0.5 * M_PI)
+          r = dist_Venus; /* we have to iterate from current distance of Venus and to ignore the rest */
+
        while(r > 0.0)
        {
           double cur_pos = dist_max - r;
@@ -155,7 +181,7 @@ int CalculateShapiro()
           double sqr_v2  = 2.0 * G * m_Sun / dist - v2_Earth; /* escape velocity at current position from sun */
           sum += 1000.0 * (sqr_v2 / (c*c - sqr_v2)); /* length increasement because of Lorentz factor */
 
-          if(sqr_v2 > 0)
+          if(sqr_v2 > 0.0)
              diff_sum += 1000.0 * dist_Sun / dist * (sqrt(1.0 + sqr_v2 / (c*c - sqr_v2)) - 1.0);
 
           if(r > 1000.0)
@@ -167,15 +193,25 @@ int CalculateShapiro()
        sum *= 2.0;
        diff_sum *= 2.0;
 
-       printf("\n");
        printf("distance to center of Sun=%.0f km (%.03f the radius of Sun)\n", dist_Sun / 1000.0, dist_Sun / r_Sun);   
-       printf("summary additional distance=%.2f m resulting delay=%.8f s\n", sum, sum / c);
-       printf("difference of distance=%.2f m resulting delay=%.8f s resulting summary delay=%.8f s\n", diff_sum, diff_sum / c, (sum - diff_sum) / c );
+       printf("summary additional distance=%.2f m resulting delay=%.2f us\n", sum, sum / c * 1000000.0);
+       printf("difference of distance=%.2f m resulting delay=%.2f us and summary delay=%.2f us\n", diff_sum, diff_sum / c * 1000000.0, (sum - diff_sum) / c * 1000000.0);
 
-       if (dist_Sun == rb_Venus)
-         dist_Sun *= 2.0;
+       if(arc == 0.0)
+       {
+          arc = -1;
+       }
        else
-         dist_Sun = rb_Venus;
+       {
+          days += 10.0;
+          arc -= 2.0 * M_PI * 10.0 * day / ys_Venus;
+
+          if(arc < 0.0)
+          {
+             days += arc / (2.0 * M_PI) * ys_Venus / day;
+             arc = 0.0;
+          }
+       }
    }
 
    iret = 1;
