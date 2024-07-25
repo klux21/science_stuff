321d0ba475a1f2461b6e5613996799ceb01d177d
Dipl.-Ing. Klaus Lux <klux21@gmx.de>
2024-07-25 14:34:47 +0000

    uses 5 day steps for time increments now

--- shapiro.c
+++ shapiro.c
@@ -12,11 +12,9 @@ exit $?
 *                                                                             *
 * --------------------------------------------------------------------------- *
 *                                                                             *
-*  DESCRIPTION:   calculation of Shapiro delay according to the difference of *
-*                 gravitational potential of a telescope on Earth and Venus   *
-*                 assuming the space is stretched according to the square of  *
-*                 the Lorentz factor of the speed of a free fall between      *
-*                 between the potentials                                      *
+*  DESCRIPTION:   calculation of Shapiro delay according to the gravitational *
+*                 potential assuming the space is stretched according to the  *
+*                 square of the Lorentz factor of the escape velocity         *
 *                                                                             *
 * --------------------------------------------------------------------------- *
 *                                                                             *
@@ -88,6 +86,8 @@ exit $?
 #include <stdio.h>
 #include <math.h>
 
+#define CHECK_DIRECTION_DEPENDENCY 0
+
 const double  year       = 31556952.0;     /* a year of 365.2425 days in seconds */
 const double  day        = 86400.0;        /* a day in seconds */
 const double  AE         = 149597870700.0; /* astromic unit in m */
@@ -114,12 +114,16 @@ int CalculateShapiro()
    double y_Venus  = 2.0 * M_PI * rb_Venus / vb_Venus;      /* calculated year of Venus */
    double y_Earth  = 2.0 * M_PI * rb_Earth / vb_Earth;      /* calculated year of Earth */
    double ys_Venus = 1.0 / (1.0 / y_Venus - 1.0 / y_Earth); /* synodic year of Venus (time it takes for Venus move around the sun from view of Earth) */
-   double dist_Sun = r_Sun + r_Venus / 2.0;                 /* minimal distance from sun the radar may work; */
+   double dist_Sun = r_Sun + r_Venus * 2.0;                 /* minimal distance from sun the radar may work; */
    double arc      = M_PI / 2.0 - asin(dist_Sun / rb_Venus) + M_PI / 2.0 - asin(dist_Sun / rb_Earth); /* angle between Earth and Venus from view of sun */
+   double days     = 0.0;                                   /* days after start */
 
-   double v2_Earth = 2.0 * G * m_Sun / rb_Earth + 2.0 * G * m_Earth / r_Earth;  /*  */
-   double r = 0.0;
-   double days     = 0;
+#if 1
+   double v2_Earth = 0.0;
+#else
+   /* enable this care about the difference in gravitational potential of the telescope on Earth and the potential on the way of light only */
+   double v2_Earth = 2.0 * G * m_Sun / rb_Earth + 2.0 * G * m_Earth / r_Earth;
+#endif
 
    printf("Synodic year of Venus in calculation=%.3f years (%.4f days)\n", ys_Venus / year, ys_Venus / day);   
    printf("Starting angle is %.4f° for passing the Sun at %.4f times the radius of the Sun\n", arc * 360.0 / 2.0 / M_PI, dist_Sun/r_Sun);   
@@ -127,18 +131,23 @@ int CalculateShapiro()
    /* calculation of Shapiro delay */
    while(arc >= 0.0)
    {
-       double sum = 0.0;
-       double diff_sum = 0.0; /* difference if using lowered tanential speed */
+       double r        = 0.0; /* current distance from Earth or Venus */
+       double sum      = 0.0;
+#if CHECK_DIRECTION_DEPENDENCY
+       double diff_sum = 0.0; /* difference if assuming lower tangential speed that some people claim */
+#endif
        double dist_max = 0.0;
-       double dist_Venus = sqrt(rb_Venus * rb_Venus + rb_Earth * rb_Earth - 2.0 * rb_Venus * rb_Earth * cos(arc)); /* distance of Venus to according the law of cosines */
+       double dist_Venus = sqrt(rb_Venus * rb_Venus + rb_Earth * rb_Earth - 2.0 * rb_Venus * rb_Earth * cos(arc)); /* distance between Earth and Venus to according the angle and the law of cosines */
 
-       dist_Sun = sin(arc) / dist_Venus * rb_Earth * rb_Venus;
+       dist_Sun = sin(arc) / dist_Venus * rb_Earth * rb_Venus; /* minimal distance of the Sun to the ray according to the law of sines and that it just depends on the sinus
+                                                                  of angle between the Sun and the other planet multiplied by the distance of the sun */
 
        printf("\n");
        /* printf("sin(as)=%.5f sin(av)=%.5f  rmax=%.3f times r_Sun\n", sin(arc), sin(arc) / dist_Venus * rb_Earth, rb_Venus / r_Sun); */
-       printf("%.0f days after starting day. Angle is %.4f°\n", days, arc * 360.0 / 2.0 / M_PI);
+       printf("%.0f days after starting day when the angle between Earth and Venus is %.4f°\n", days, arc * 360.0 / 2.0 / M_PI);
        printf("distance of Venus is %.0f km\n", dist_Venus / 1000.0);
        printf("%s the Sun at %.4f times the radius of the Sun\n", arc >= (0.5 * M_PI) ? "passing" : "would passing", dist_Sun/r_Sun);   
+
 
        if(arc <= (0.5 * M_PI))
           dist_max = 0.0; /* we need to skip this */
@@ -153,8 +162,10 @@ int CalculateShapiro()
           double sqr_v2  = 2.0 * G * m_Sun / dist - v2_Earth; /* escape velocity at current position from sun */
           sum += 1000.0 * (sqr_v2 / (c*c - sqr_v2)); /* length increasement because of Lorentz factor */
 
+#if CHECK_DIRECTION_DEPENDENCY
           if(sqr_v2 > 0.0)
              diff_sum += 1000.0 * dist_Sun / dist * (sqrt(1.0 + sqr_v2 / (c*c - sqr_v2)) - 1.0);
+#endif
 
           if(r > 1000.0)
              r -= 1000.0;
@@ -181,9 +192,10 @@ int CalculateShapiro()
           double sqr_v2  = 2.0 * G * m_Sun / dist - v2_Earth; /* escape velocity at current position from sun */
           sum += 1000.0 * (sqr_v2 / (c*c - sqr_v2)); /* length increasement because of Lorentz factor */
 
+#if CHECK_DIRECTION_DEPENDENCY
           if(sqr_v2 > 0.0)
              diff_sum += 1000.0 * dist_Sun / dist * (sqrt(1.0 + sqr_v2 / (c*c - sqr_v2)) - 1.0);
-
+#endif
           if(r > 1000.0)
              r -= 1000.0;
           else
@@ -191,11 +203,13 @@ int CalculateShapiro()
        }
 
        sum *= 2.0;
-       diff_sum *= 2.0;
-
        printf("distance to center of Sun=%.0f km (%.03f the radius of Sun)\n", dist_Sun / 1000.0, dist_Sun / r_Sun);   
        printf("summary additional distance=%.2f m resulting delay=%.2f us\n", sum, sum / c * 1000000.0);
+
+#if CHECK_DIRECTION_DEPENDENCY
+       diff_sum *= 2.0;
        printf("difference of distance=%.2f m resulting delay=%.2f us and summary delay=%.2f us\n", diff_sum, diff_sum / c * 1000000.0, (sum - diff_sum) / c * 1000000.0);
+#endif
 
        if(arc == 0.0)
        {
@@ -203,8 +217,8 @@ int CalculateShapiro()
        }
        else
        {
-          days += 10.0;
-          arc -= 2.0 * M_PI * 10.0 * day / ys_Venus;
+          days += 5.0;
+          arc -= 2.0 * M_PI * 5.0 * day / ys_Venus;
 
           if(arc < 0.0)
           {
